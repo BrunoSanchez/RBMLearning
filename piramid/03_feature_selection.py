a71fe446588ecca02c835be13fb567b3c8ea56eb
brunosanchez <bruno@oac.unc.edu.ar>
2018-11-04 01:55:25 -0300

    working on feature selection

---
+++ piramid/03_feature_selection.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  03_feature_selection.py
+#
+#  Copyright 2018 Bruno S <bruno@oac.unc.edu.ar>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
+import os
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from astropy.stats import sigma_clipped_stats
+
+from sklearn import preprocessing
+from sklearn import decomposition
+from sklearn import feature_selection
+from sklearn import svm
+from sklearn import metrics
+from sklearn import neighbors
+from sklearn.svm import SVC
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.externals import joblib
+
+from sklearn.model_selection import KFold
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from rfpimp import *
+
+import custom_funs as cf
+
+
+storefile = '/mnt/clemente/bos0109/table_store.h5'
+
+store = pd.HDFStore(storefile)
+store.open()
+
+#sns.set_context(font_scale=16)
+plt.rcParams["patch.force_edgecolor"] = True
+plt.rcParams['text.usetex'] = True
+
+
+def main(m1_diam=1.54, plots_path='./plots/.'):
+    plot_dir = os.path.abspath(plots_path)
+    if not os.path.isdir(plot_dir):
+        os.makedirs(plot_dir)
+
+    simulated = store['simulated']
+    simulations = store['simulations']
+
+    simulations = simulations[simulations.failed_to_subtract==False]
+    if m1_diam is not None:
+        simulations = simulations[simulations.m1_diam==m1_diam]
+
+    simus = pd.merge(left=simulations, right=simulated,
+                     right_on='simulation_id', left_on='id', how='outer')
+
+    # =============================================================================
+    # tables
+    # =============================================================================
+    dt_zps = store['dt_ois']
+    if m1_diam is not None:
+        dt_zps = dt_zps[dt_zps.m1_diam==m1_diam]
+    dt_zps = cf.optimize_df(dt_zps)
+    dt_ois = dt_zps
+
+    dt_zps = store['dt_sps']
+    if m1_diam is not None:
+        dt_zps = dt_zps[dt_zps.m1_diam==m1_diam]
+    dt_zps = cf.optimize_df(dt_zps)
+    dt_sps = dt_zps
+
+    dt_zps = store['dt_hot']
+    if m1_diam is not None:
+        dt_zps = dt_zps[dt_zps.m1_diam==m1_diam]
+    dt_zps = cf.optimize_df(dt_zps)
+    dt_hot = dt_zps
+
+    dt_zps = store['dt_zps']
+    if m1_diam is not None:
+        dt_zps = dt_zps[dt_zps.m1_diam==m1_diam]
+    dt_zps = cf.optimize_df(dt_zps)
+
+    dt_ois['MU'] = dt_ois.MAG/(dt_ois.A_IMAGE*dt_ois.B_IMAGE)
+    dt_zps['MU'] = dt_zps.MAG/(dt_zps.A_IMAGE*dt_zps.B_IMAGE)
+    dt_hot['MU'] = dt_hot.MAG/(dt_hot.A_IMAGE*dt_hot.B_IMAGE)
+    dt_sps['MU'] = dt_sps.MAG/(dt_sps.a*dt_sps.b)
+
+    dt_ois['SN'] = dt_ois.FLUX_APER/dt_ois.FLUXERR_APER
+    dt_zps['SN'] = dt_zps.FLUX_APER/dt_zps.FLUXERR_APER
+    dt_hot['SN'] = dt_hot.FLUX_APER/dt_hot.FLUXERR_APER
+    dt_sps['SN'] = dt_sps.cflux/np.sqrt(dt_sps.cflux)
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--m1_diam", help="diameter to filter",
+                        default=None)
+    parser.add_argument("path", help="path to plot files")
+    args = parser.parse_args()
+
+    import sys
+    sys.exit(main(args.m1_diam, args.path))
