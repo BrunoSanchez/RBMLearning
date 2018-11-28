d20cdc680deb4265e3f0fd60a4e1e5d1e2a74c3d
brunosanchez <bruno@oac.unc.edu.ar>
2018-11-28 18:58:42 -0300

    trying to write the magic ML function

--- piramid/custom_funs.py
+++ piramid/custom_funs.py
@@ -28,7 +28,6 @@ from sklearn.feature_selection import mutual_info_classif
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import VarianceThreshold
 
-
 from rfpimp import *
 
 # We're going to be calculating memory usage a lot,
@@ -212,61 +211,6 @@ def custom_histogram(vector, bins=None, cumulative=False, errors=False):
         return x_bins, hh[0]
 
 
-def experiment(clf, x, y, nfolds=10, printing=False, probs=True,
-               train_final=False):
-    # import ipdb; ipdb.set_trace()
-    skf = StratifiedKFold(n_splits=nfolds)
-    probabilities = None # np.array([])
-    predictions = np.array([])
-    y_testing = np.array([])
-
-    results = {}
-    for train, test in skf.split(x, y):
-
-        x_train = x[train]
-        y_train = y[train]
-        clf.fit(x_train, y_train)
-
-        x_test = x[test]
-        y_test = y[test]
-        pr = clf.predict(x_test)
-        if probs:
-            probs = clf.predict_proba(x_test)  #[:, 0]
-
-            probabilities = (
-                probs if probabilities is None else
-                np.vstack([probabilities, probs]))
-        predictions = np.hstack([predictions, pr])
-        y_testing = np.hstack([y_testing, y_test])
-
-    results['y_test'] = y_testing
-    results['predictions'] = predictions
-    if probs:
-        results['probabilities'] = probabilities
-
-    if printing:
-        print(metrics.classification_report(y_testing, predictions))
-
-    if probs:
-        fpr, tpr, thresholds = metrics.roc_curve(y_testing, 1.-probabilities[:, 0])
-        prec_rec_curve = metrics.precision_recall_curve(y_testing, 1.- probabilities[:, 0])
-        roc_auc = metrics.auc(fpr, tpr)
-
-        results['fpr'] = fpr
-        results['tpr'] = tpr
-        results['thresh'] = thresholds
-        results['roc_auc'] = roc_auc
-        results['prec_rec_curve'] = prec_rec_curve
-
-    if train_final:
-        clf.fit(x, y)
-
-    results['model'] = clf
-    results['confusion_matrix'] = metrics.confusion_matrix(y_testing, predictions)
-
-    return results
-
-
 from sklearn.linear_model import RANSACRegressor
 
 def get_mags_iso(df):
@@ -337,8 +281,9 @@ def cal_mags(df):
                       columns=['image_id', 'mean_offset', 'slope', 'p05', 'p95'])
     return dd
 
-from sklearn.ensemble import ExtraTreesClassifier
-
+# =============================================================================
+# Feature selectors
+# =============================================================================
 def importance_forest(X, y, forest=None, cols=None, method=None):
     if forest is None:
         forest = ExtraTreesClassifier(n_estimators=250,
@@ -421,7 +366,6 @@ def importance_perm_kfold(X, y, forest=None, cols=None, method=None, nfolds=10):
     #imp = pd.concat(imp, axis=1)
     return imp
 
-
 def select(X, Y, percentile, selector_f=mutual_info_classif, log=False):
     selector = SelectPercentile(selector_f, percentile=percentile)
     selector.fit(X, Y)
@@ -476,6 +420,64 @@ def importance_perm_kfold(X, y, forest=None, cols=None, method=None, nfolds=10):
     return imp
 
 # =============================================================================
+# Experiment
+# =============================================================================
+def experiment(clf, x, y, nfolds=10, printing=False, probs=True,
+               train_final=False):
+    # import ipdb; ipdb.set_trace()
+    skf = StratifiedKFold(n_splits=nfolds)
+    probabilities = None # np.array([])
+    predictions = np.array([])
+    y_testing = np.array([])
+
+    results = {}
+    for train, test in skf.split(x, y):
+
+        x_train = x[train]
+        y_train = y[train]
+        clf.fit(x_train, y_train)
+
+        x_test = x[test]
+        y_test = y[test]
+        pr = clf.predict(x_test)
+        if probs:
+            probs = clf.predict_proba(x_test)  #[:, 0]
+
+            probabilities = (
+                probs if probabilities is None else
+                np.vstack([probabilities, probs]))
+        predictions = np.hstack([predictions, pr])
+        y_testing = np.hstack([y_testing, y_test])
+
+    results['y_test'] = y_testing
+    results['predictions'] = predictions
+    if probs:
+        results['probabilities'] = probabilities
+
+    if printing:
+        print(metrics.classification_report(y_testing, predictions))
+
+    if probs:
+        fpr, tpr, thresholds = metrics.roc_curve(y_testing, 1.-probabilities[:, 0])
+        prec_rec_curve = metrics.precision_recall_curve(y_testing, 1.- probabilities[:, 0])
+        roc_auc = metrics.auc(fpr, tpr)
+
+        results['fpr'] = fpr
+        results['tpr'] = tpr
+        results['thresh'] = thresholds
+        results['roc_auc'] = roc_auc
+        results['prec_rec_curve'] = prec_rec_curve
+
+    if train_final:
+        clf.fit(x, y)
+
+    results['model'] = clf
+    results['confusion_matrix'] = metrics.confusion_matrix(y_testing, predictions)
+
+    return results
+
+
+# =============================================================================
 # funcion para ml
 # =============================================================================
 def group_ml(train_data, group_cols=['m1_diam', 'exp_time', 'new_fwhm'],
@@ -483,60 +485,65 @@ def group_ml(train_data, group_cols=['m1_diam', 'exp_time', 'new_fwhm'],
              method='Bramich'):
     rows = []
     for pars, data in train_data.groupby(group_cols):
+
+        ## spliting the data into train and final test
         train, test = train_test_split(data[cols+target].dropna(), test_size=0.25,
                                        stratify=data[cols+target].dropna().IS_REAL)
-        d_ois = train[cols]
-        y_ois = train[target]
+        d = train[cols]
+        y = train[target]
 
-        scaler = preprocessing.StandardScaler().fit(d_ois)
-        X_ois = scaler.transform(d_ois)
-        X_test_ois = scaler.transform(test[cols])
+        scaler = preprocessing.StandardScaler().fit(d)
+        X = scaler.transform(d)
+        X_test = scaler.transform(test[cols])
 
         # =============================================================================
-        # univariate
+        # univariate cuts
         # =============================================================================
         thresh = var_thresh
         sel = VarianceThreshold(threshold=thresh)
-        X_ois = sel.fit_transform(X_ois)
-        X_test_ois = sel.transform(X_test_ois)
-        newcols_ois = d_ois.columns[sel.get_support()]
-        print('Dropped columns = {}'.format(d_ois.columns[~sel.get_support()]))
-        d_ois = pd.DataFrame(X_ois, columns=newcols_ois)
+        X = sel.fit_transform(X)
+        X_test = sel.transform(X_test)
+        newcols = d.columns[sel.get_support()]
+        print('Dropped columns = {}'.format(d.columns[~sel.get_support()]))
+        d = pd.DataFrame(X, columns=newcols)
 
         percentile = percentile
-        scores, selector, selected_cols = select(X_ois, y_ois, percentile)
-        scoring_ois = pd.DataFrame(scores, index=newcols_ois, columns=['ois'])
-        selection_ois = scoring_ois.loc[newcols_ois.values[selected_cols][0]]
-        dat_ois = pd.DataFrame(X_ois, columns=newcols_ois)[selection_ois.index]
+        scores, selector, selected_cols = select(X, y, percentile)
+        scoring = pd.DataFrame(scores, index=newcols, columns=[method])
+        selection = scoring.loc[newcols.values[selected_cols][0]]
+        dat = pd.DataFrame(X, columns=newcols)[selection.index]
 
         # =============================================================================
         # KNN
         # =============================================================================
         model = neighbors.KNeighborsClassifier(n_neighbors=7, weights='uniform', n_jobs=-1)
 
-        rslt0_knn_ois_uniform = experiment(model, X_ois, y_ois.values.ravel(), printing=True)
-        model.fit(X_ois, y_ois.values.ravel())
-        preds = model.predict(X_test_ois)
-        rslt0_knn_ois_uniform['test_preds'] = preds
+        # experiment befor fslection
+        rslt0_knn = experiment(model, X, y.values.ravel(), printing=false)
+        # experiment after fselection
+        rslts_knn = experiment(model, dat.values, y.values.ravel(), printing=false)
+
+
+        model.fit(X, y.values.ravel())
+        preds = model.predict(X_test)
+        rslt0_knn['test_preds'] = preds
+
         print(metrics.classification_report(test.IS_REAL.values.ravel(), preds))
         acc_knn0 = metrics.accuracy_score(test.IS_REAL.values.ravel(), preds)
-        rslt0_knn_ois_uniform['test_bacc'] = acc_knn0
+        rslt0_knn['test_bacc'] = acc_knn0
 
-        rslts_knn_ois_uniform = experiment(model, dat_ois.values, y_ois.values.ravel(), printing=True)
-        model.fit(dat_ois.values, y_ois.values.ravel())
-        preds = model.predict(selector.transform(X_test_ois))
-        rslts_knn_ois_uniform['test_preds'] = preds
+        model.fit(dat.values, y.values.ravel())
+        preds = model.predict(selector.transform(X_test))
+        rslts_knn['test_preds'] = preds
+
         print(metrics.classification_report(test.IS_REAL.values.ravel(), preds))
         acc_knn = metrics.accuracy_score(test.IS_REAL.values.ravel(), preds)
-        rslt0_knn_ois_uniform['test_bacc'] = acc_knn
+        rslt0_knn['test_bacc'] = acc_knn
 
         # =============================================================================
         # randomforest
         # =============================================================================
-        corr = d_ois.corr()
-        # Generate a mask for the upper triangle
-        mask = np.zeros_like(corr, dtype=np.bool)
-        mask[np.triu_indices_from(mask)] = True
+        corr = d.corr()
         # remove corr columns
         correlated_features = set()
         for i in range(len(corr.columns)):
@@ -544,48 +551,54 @@ def group_ml(train_data, group_cols=['m1_diam', 'exp_time', 'new_fwhm'],
                 if abs(corr.iloc[i, j]) > 0.8:
                     colname = corr.columns[i]
                     correlated_features.add(colname)
-        decorr_ois = d_ois.drop(correlated_features, axis=1)
-        corr = decorr_ois.corr()
+        decorr = d.drop(correlated_features, axis=1)
+        corr = decorr.corr()
 
         model = RandomForestClassifier(n_estimators=400, random_state=0, n_jobs=-1)
-        ois_importance = importance_perm_kfold(decorr_ois.values, y_ois.values.ravel(),
-            model, cols=decorr_ois.columns, method=method)
+        importance = importance_perm_kfold(decorr.values, y.values.ravel(),
+            model, cols=decorr.columns, method=method)
 
-        res_ois = pd.concat(ois_importance, axis=1)
-        full_cols = list(decorr_ois.index).extend(['Random'])
-        m = res_ois.mean(axis=1).reindex(full_cols)
-        s = res_ois.std(axis=1).reindex(full_cols)
+        res = pd.concat(importance, axis=1)
+        full_cols = list(decorr.index).extend(['Random'])
+        m = res.mean(axis=1).reindex(full_cols)
+        s = res.std(axis=1).reindex(full_cols)
 
         thresh = m.loc['Random'] + 3*s.loc['Random']
         spikes = m - 3*s
         selected = spikes > thresh
         signif = (m - m.loc['Random'])/s
         selected = signif>2.5
-        dat_ois = d_ois[selected[selected].index]
+        dat = d[selected[selected].index]
 
-        n_fts = np.min([len(dat_ois.columns), 7])
+        n_fts = np.min([len(dat.columns), 7])
         model = RandomForestClassifier(n_estimators=800, max_features=n_fts,
                                        min_samples_leaf=20, n_jobs=-1)
-        rslts_ois_rforest = experiment(model, dat_ois.values, y_ois.values.ravel(), printing=True)
-        model.fit(dat_ois.values, y_ois.values.ravel())
-        d_test = pd.DataFrame(X_test_ois, columns=newcols_ois)[selected[selected].index]
+
+        # experiment before fselection
+        rslt0_ois_rforest = experiment(model, X, y.values.ravel(), printing=True)
+        # experiment after fselection
+        rslts_rforest = experiment(model, dat.values, y.values.ravel(), printing=True)
+
+
+        model.fit(dat.values, y.values.ravel())
+
+        d_test = pd.DataFrame(X_test, columns=newcols_ois)[selected[selected].index]
         preds = model.predict(d_test.values)
         #rslts_ois_rforest['test_preds'] = preds
         print(metrics.classification_report(test.IS_REAL.values.ravel(), preds))
         acc_rforest = cf.metrics.accuracy_score(test.IS_REAL.values.ravel(), preds)
         #rslts_ois_rforest['test_bacc'] = acc_rforest
 
-        rslt0_ois_rforest = experiment(model, d_ois.values, y_ois.values.ravel(), printing=True)
-        model.fit(d_ois.values, y_ois.values.ravel())
-        preds = model.predict(X_test_ois)
+        model.fit(d.values, y.values.ravel())
+        preds = model.predict(X_test)
         # rslt0_ois_rforest['test_preds'] = preds
         print(metrics.classification_report(test.IS_REAL.values.ravel(), preds))
         acc_rforest0 = metrics.accuracy_score(test.IS_REAL.values.ravel(), preds)
         # rslt0_ois_rforest['test_bacc'] = acc_rforest0
 
-# =============================================================================
-# SVC
-# =============================================================================
+        # =============================================================================
+        # SVC
+        # =============================================================================
         #svc = SVC(kernel='linear',
         #          cache_size=2048,
         #          class_weight='balanced',
@@ -594,29 +607,29 @@ def group_ml(train_data, group_cols=['m1_diam', 'exp_time', 'new_fwhm'],
         rfecv = feature_selection.RFECV(estimator=svc, step=1, cv=StratifiedKFold(6),
                       scoring='accuracy', n_jobs=-1)
 
-        rfecv.fit(np.ascontiguousarray(X_ois), y_ois.values.ravel())
+        rfecv.fit(np.ascontiguousarray(X), y.values.ravel())
         print("Optimal number of features : {}" .format(rfecv.n_features_))
-        sel_cols_ois = newcols_ois[rfecv.support_]
-        print(sel_cols_ois)
-        dat_ois = d_ois[sel_cols_ois]
+        sel_cols = newcols[rfecv.support_]
+        print(sel_cols)
+        dat = d[sel_cols]
 
         model = svc
-        rslts_ois_svc = cf.experiment(model, dat_ois.values, y_ois.values.ravel(), printing=True, probs=False)
-        model.fit(dat_ois.values, y_ois.values.ravel())
-        preds = model.predict(pd.DataFrame(X_test_ois, columns=newcols_ois)[sel_cols_ois].values)
+        rslts_svc = cf.experiment(model, dat.values, y.values.ravel(),
+                                  printing=False, probs=False)
+        model.fit(dat.values, y.values.ravel())
+        preds = model.predict(pd.DataFrame(X_test, columns=newcols)[sel_cols].values)
         #rslts_ois_svc['test_preds'] = preds
         print(cf.metrics.classification_report(test.IS_REAL.values.ravel(), preds))
         acc_svc = cf.metrics.accuracy_score(test.IS_REAL.values.ravel(), preds)
         #rslts_ois_svc['test_bacc'] = acc_svc
 
-        rslt0_ois_svc = cf.experiment(model, d_ois.values, y_ois.values.ravel(), printing=True)
-        model.fit(d_ois.values, y_ois.values.ravel())
-        preds = model.predict(X_test_ois)
+        rslt0_svc = cf.experiment(model, d.values, y.values.ravel(), printing=True)
+        model.fit(d.values, y.values.ravel())
+        preds = model.predict(X_test)
         #rslt0_ois_svc['test_preds'] = preds
         print(cf.metrics.classification_report(test.IS_REAL.values.ravel(), preds))
         acc_svc0 = cf.metrics.accuracy_score(test.IS_REAL.values.ravel(), preds)
         #rslt0_ois_svc['test_bacc'] = acc_svc0
-
 
         vals = [acc_knn0, acc_knn, acc_rforest0, acc_rforest, acc_svc, acc_svc0]
         rows.append(list(pars)+vals)
