b9736fb84dce19a574fab02a0cacb40dbfedf159
brunosanchez <bruno@oac.unc.edu.ar>
2018-12-12 16:20:19 -0300

    stupid bug

--- piramid/custom_funs.py
+++ piramid/custom_funs.py
@@ -894,12 +894,12 @@ def group_ml(train_data, und, group_cols=['m1_diam', 'exp_time', 'new_fwhm'],
                     test_f1_svc]
         row_svc += list(final_cm.flatten()) + [TP, FP, FN, P, R, F1]
 
-        tracers.append(pd.Dataframe([ids, y, y_pred_knn0, y_pred_knn, y_pred_rfo0, y_pred_rfo,
+        tracers.append(pd.DataFrame([ids, y, y_pred_knn0, y_pred_knn, y_pred_rfo0, y_pred_rfo,
                       y_pred_svc0, y_pred_svc], columns=['id', 'y', 'y_pred_knn0',
                       'y_pred_knn', 'y_pred_rfo0', 'y_pred_rfo', 'y_pred_svc0',
                       'y_pred_svc']))
 
-        tracers.append(pd.Dataframe([ids_test, y_test, y_pred_test_knn0,
+        tracers.append(pd.DataFrame([ids_test, y_test, y_pred_test_knn0,
                                      y_pred_test_knn, y_pred_test_rfo0,
                                      y_pred_test_rfo, y_pred_test_svc0,
                                      y_pred_test_svc], columns=['id', 'y', 'y_pred_knn0',
